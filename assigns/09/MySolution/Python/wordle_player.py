35b3507bcf13c05975f0e7f5d033f6e96cb66f98
cccuirq <123427931+cccuirq@users.noreply.github.com>
2023-04-27 23:21:02 -0400

    Update wordle_player.py

--- assigns/09/MySolution/Python/wordle_player.py
+++ assigns/09/MySolution/Python/wordle_player.py
@@ -9,29 +9,39 @@ import queue
 import sys
 sys.path.append('./../../../../mypylib')
 from mypylib_cls import *
-
+import nltk
+nltk.download('words')
+from nltk.corpus import words
 ########################################################################
-def replace(guess, x, y):
-    return guess[:x] + y + guess[x+1:]
-
 def dfs(n, nx):
     checked = set()
+    stack = []
+    results =[]
+    
+    for n1 in n:
+        stack.append(n1)
+        checked.add(n1)
+    
+    while stack:
+        current = stack.pop()
+        results.append(current)
 
-    def helper(cc):
-        if cc.empty() == True:
-            return strcon_nil()
-        else:
-            n1 = cc.get()
-            for n2 in reversed(nx(n1)):
-                if (not n2 in checked):
-                    cc.put(n2)
-                    checked.add(n2)
-                    return strcon_cons(n1, lambda: helper(cc))
-    cnx = queue.LifoQueue()
-    for n0 in n:
-        cnx.put(n0)
-        checked.add(n0)
+        for next in reversed(nx(current)):
+            if next not in checked:
+                stack.append(next)
+                checked.add(next)
+    return results
 
+def next(nxt1, still_check_count):
+            results = []
+            try:
+                index = nxt1.index("_")
+                for l in still_check_count:
+                    results.append(nxt1[:index] + l + nxt1[index+1:])
+                return results
+            except ValueError:
+                return []
+            
 def wordle_guess(hints):
     guess = "_" * len(hints[0])
     still_check_count = []
@@ -41,36 +51,26 @@ def wordle_guess(hints):
     for hint in hints:
         for x,y in enumerate(hint):
             if y[0] == 1:
-                guess = replace(guess, x, y[1])
+                guess = guess[:x] + y[1] + guess[x+1:]
             elif y[0] == 2:
                 still_check.add((x, y[1]))
                 still_check_count += y[1]
             else:
                 if y[1] in dict:
                     dict.remove(y[1])
-    def result_check(word):
-        wordlist = list(word)
-        def check1(i):
-            return foreach_to_iforall(string_foreach)(i,lambda n,x: (not(n,x) in i))
-        def check2(i):
-            result = True
-            for cc in still_check_count:
-                if (cc in i):
-                    i.remove(cc)
-                else:
-                    result = False
-                    break
-                return result
-            return check1(word) and check2(wordlist) and (not '_' in word)
-        def next(nxt1):
-            try:
-                i = nxt1.index('_')
-                return string_imap_pylist(dict, lambda _,c: replace(nxt1, i, c))
-            except ValueError:
-                return []
-    if '_' in guess:
-        g1 = stream_make_filter(dfs([guess], next), lambda s: result_check(s))
-        return stream_get_at(g1, 0)
+    def result_check(word, wrong, included):
+        for i, l in enumerate(word):
+            if (i,l) in wrong:
+                return False
+        for l in included:
+            if l not in word:
+                return False
+        return "_" not in word and word in words.words()
+    if "_" in guess:
+        valid_guesses = dfs([guess], lambda x: next(x, still_check_count))
+        for word in valid_guesses:
+            if result_check(word, still_check, still_check_count):
+                return word
     else:
         return guess
 ########################################################################
